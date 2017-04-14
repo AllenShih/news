ee75c253d48b84edb510578e88440fac3e01f901
Allen <kingjames1324@gmail.com>
2017-04-14 16:00:29 +0800

    href

--- appledaily.py
+++ appledaily.py
@@ -11,11 +11,11 @@ all=soup.find_all("div",{"class":"abdominis rlby clearmen"})
 
 hr="http://www.appledaily.com.tw/"
 for article in all:
-    news=article.find_all("li",{"class":"rtddt life"} )+article.find_all("li",{"class":"rtddt life even"} )+article.find_all("li",{"class":"rtddt life enen hsv"} )
+    news=article.find_all("li",{"class":"rtddt life"}) +article.find_all("li",{"class":"rtddt life even"}) +article.find_all("li",{"class":"rtddt life enen hsv"} )
     for lines in news:
         print(lines.text)
         links=lines.find_all("a")
-        # for href in links:
-        #     href=href.get('href')
-        #     Entire_link=hr+href
-        #     print(Entire_link)
+        for href in links:
+            href=href.get('href')
+            Entire_link=hr+href
+            print(Entire_link)

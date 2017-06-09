72c676a25f9fc8370466f27b486a5e994bbf33d2
Allen <kingjames1324@gmail.com>
2017-06-09 18:29:52 +0800

    adding road sheet

--- database_check.py
+++ database_check.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from database import Database
+# from database import Database
 
-from datetime import date
+# from datetime import date
 
 # dbname = " dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432' "
 
@@ -14,5 +14,12 @@ from datetime import date
 # l=database.search(title="(151551))")
 # print(l)
 
-dat= "2017 / 06 / 02 12:21"
-print(dat.replace(" / ","-"))
+# dat= "2017 / 06 / 02 12:21"
+# print(dat.replace(" / ","-"))
+
+import pandas as pd
+
+df = pd.read_csv("/Users/AllenShih/Desktop/news/%E7%9C%81%E9%81%93%E5%BA%A7%E6%A8%99.csv")
+
+print(df)
+
